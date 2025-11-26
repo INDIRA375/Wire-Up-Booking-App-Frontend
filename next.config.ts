b172d464774c685a3efb0e2f7027327051fcb85e
unknown <indirabala2000@gmail.com>
2025-11-26 19:34:43 +0530

    Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizeCss: false, // Disable LightningCSS to fix Render build error
+  },
 };
 
 export default nextConfig;
